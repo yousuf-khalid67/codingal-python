0cb7118e6273db3f171b86bd9bfe57a2514015c8
yousuf-khalid67 <Khalid.yousuf2013@gmail.com>
2025-12-20 19:56:43 +1100

    Your message

---
+++ M2/L5/homework.py
@@ -0,0 +1,5 @@
+rows = 5
+for i in range(1, rows + 1):
+    print("*" * i)
+for i in range(rows, 0, -1):
+    print("*" * i)
---
+++ M2/L6/homework.py
@@ -0,0 +1,15 @@
+import turtle    #importing library
+turtle.Screen().bgcolor("orange")
+turtle.Screen().setup(500,350)
+polygon = turtle.Turtle() #defined variable
+ 
+num_sides = 5000000000000000000000000 #variable
+side_length = 60
+angle = 360.0 / num_sides
+#iterate loop for total number of side
+for i in range(num_sides):
+    polygon.forward(side_length)
+    polygon.right(angle)
+     
+turtle.done()
+
