d33f747067f86198c1f4550da9a81b644e4e2a89
yousuf-khalid67 <Khalid.yousuf2013@gmail.com>
2025-12-27 20:08:15 +1100

    Your message

--- M3/L1/basic_calculator.py
+++ M3/L1/basic_calculator.py
@@ -1,7 +1,14 @@
 def sum (a, b): 
     return a+b
-
-
+def sub (a, b): 
+    return a-b
+def multiple (a, b):
+    return a*b
+def div (a, b):
+    return a/b
 a = int(input("enter 1st number: "))
 b = int(input("enter 2nd number: "))
 print(f"sum of {a} and {b} is {sum(a, b)}")
+print(f"multiple of {a} and {b} is {multiple(a, b)}")
+print(f"div of {a} and {b} is {div(a, b)}")
+print(f"sub of {a} and {b} is {sub(a, b)}")
