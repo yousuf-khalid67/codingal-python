db9121caa9ee55b1c451cbebd0c279bb460107e4
yousuf-khalid67 <Khalid.yousuf2013@gmail.com>
2026-01-17 20:22:38 +1100

    CW or HW

---
+++ M3/L9/frequency.py
@@ -0,0 +1,18 @@
+test_dict = {'Codingal' : 2, 'is' : 2, 'best' : 2, 'for' : 3, 'Coding' : 1}
+  
+# printing original dictionary
+print("The original dictionary : " +  str(test_dict))
+  
+# Initialize value 
+K = 2
+  
+# Using loop
+# Selective key values in dictionary
+res = 0
+for key in test_dict:
+    if test_dict[key] == K:
+        res = res + 1
+      
+# printing result 
+print("Frequency of K is : " + str(res))
+
